37236641f385228a75c37da6b449da76210af57d
Ivan <ivan.i.n@hotmail.com>
2016-11-20 19:20:23 -0200

    limpesa de log

--- src/main/java/org/spingarda/features/Protector.java
+++ src/main/java/org/spingarda/features/Protector.java
@@ -60,6 +60,7 @@ public class Protector {
 
                 if (params.getKeepRunning() != null && params.getKeepRunning()) {
                     Thread.sleep(params.getKeepRunningInterval());
+                    setLog("");
                 }
             } while (params.getKeepRunning() != null && params.getKeepRunning());
 
